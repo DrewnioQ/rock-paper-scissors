4f398aefe397c65379bf7b6e20e7cc1deb71a603
DrewnioQ <debski.dev@gmail.com>
2023-03-06 16:38:01 +0100

    Add updateScore and checkRoundWinner functions

--- script-ui.js
+++ script-ui.js
@@ -6,6 +6,17 @@
 
 // 4. Function game() takes roundNum as an argument. Then it creates a for loop that ends, when roundNum is reached. Each loop prompts user for their choice, generates computer choice and plays a round
 
+const playerScoreText = document.getElementById("playerScore");
+const playerScoreSign = document.getElementById("playerSign");
+
+const computerScoreText = document.getElementById("computerScore");
+const computerScoreSign = document.getElementById("computerSign");
+
+const roundResultMessage = document.getElementById("roundResultMessage");
+const roundResultExplanation = document.getElementById(
+  "roundResultExplanation"
+);
+
 const buttons = document.querySelectorAll(".button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -13,26 +24,30 @@ buttons.forEach((button) => {
   });
 });
 
+let roundNumber = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 const weapons = {
   Rock: {
-    name: "Rock",
     id: "rockBtn",
+    icon: "🪨",
     weakTo: "Paper",
     strongTo: "Scissors",
     strongMsg: "Rock beats Scissors",
     weakMsg: "Paper beats Rock",
   },
   Paper: {
-    name: "Paper",
     id: "paperBtn",
+    icon: "📄",
     weakTo: "Scissors",
     strongTo: "Rock",
     strongMsg: "Paper beats Rock",
     weakMsg: "Scissors beat Paper",
   },
   Scissors: {
-    name: "Scissors",
     id: "scissorsBtn",
+    icon: "✂️",
     weakTo: "Rock",
     strongTo: "Paper",
     strongMsg: "Scissors beats Paper",
@@ -43,19 +58,53 @@ const weapons = {
 function getComputerChoice() {
   const choices = Object.keys(weapons);
   const rndNumber = Math.floor(Math.random() * choices.length);
-  return weapons[choices[rndNumber]].name;
+  return choices[rndNumber];
 }
 
 function getPlayerChoice(button) {
   for (let weapon in weapons) {
     if (weapons[weapon].id === button.id) {
-      return weapons[weapon].name;
+      return weapon;
     }
   }
   return null;
 }
 
+function updateScore(computerChoice, playerChoice) {
+  playerScoreSign.classList.remove("rotate180");
+  computerScoreSign.classList.remove("rotate180");
+
+  if (playerChoice === "Scissors") playerScoreSign.classList.add("rotate180");
+  playerScoreSign.textContent = weapons[playerChoice].icon;
+  playerScoreText.textContent = `Player: ${playerScore}`;
+
+  if (computerChoice === "Scissors")
+    computerScoreSign.classList.add("rotate180");
+  computerScoreSign.textContent = weapons[computerChoice].icon;
+  computerScoreText.textContent = `Computer: ${computerScore}`;
+}
+
+function checkRoundWinner(computerChoice, playerChoice) {
+  if (weapons[playerChoice].strongTo === computerChoice) {
+    roundResultMessage.textContent = `You win 😃`;
+    roundResultExplanation.textContent = `${weapons[playerChoice].strongMsg}`;
+    playerScore++;
+  }
+  if (weapons[playerChoice].weakTo === computerChoice) {
+    roundResultMessage.textContent = `You lose 😢`;
+    roundResultExplanation.textContent = `${weapons[playerChoice].weakMsg}`;
+    computerScore++;
+  }
+  if (playerChoice === computerChoice) {
+    roundResultMessage.textContent = `TIE 🤨`;
+    roundResultExplanation.textContent = `Keep playing!`;
+  }
+}
+
 function playRound(button) {
   const computerChoice = getComputerChoice();
   const playerChoice = getPlayerChoice(button);
+
+  checkRoundWinner(computerChoice, playerChoice);
+  updateScore(computerChoice, playerChoice);
 }
